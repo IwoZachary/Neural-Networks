11a5fb63a9f5ef3d67b614f0e7f9318b64bdb62a
IwoZachary <61707966+IwoZachary@users.noreply.github.com>
2021-11-05 09:53:45 +0100

    Training implemented

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-#from keras.datasets import mnist
-#import numpy as np
+from keras.datasets import mnist
+import numpy as np
 
-from mlp import MLP
+from mlpv2 import MLPv2
 
-#(train_X, train_y), (test_X, test_y) = mnist.load_data()
-mlp =MLP([[1, 2, 3, 4]], [], [3,2,4])
+(train_X, train_y), (test_X, test_y) = mnist.load_data()
+a = []
+for i in range (30):
+    a.append(np.ravel(train_X[i]))
+a = np.array(a)
+print(a.shape)
+print(train_y.shape)
 
+mlp =MLPv2(784, [800, 500])
 
-print(mlp.forward_propagate([1, 2, 3, 4]))
+mlp.train(a, train_y)
--- mlpv2.py
+++ mlpv2.py
@@ -4,7 +4,7 @@ import copy
 NUMBER_OF_OUTPUTS = 10
 
 
-class MLP:
+class MLPv2:
     def __init__(self, num_inputs, hidden_layers):
 
         self.num_inputs = num_inputs  # ilość danych wejściowych
@@ -26,25 +26,52 @@ class MLP:
         self.activation = activation
 
     def forward_propagate(self, input_x):
+        #print(input_x)
         activation = input_x  # warstwa wejściowa plus bias
-        self.activation[0] = activation
+        self.activation[0] = np.array(activation)
         for i, weight in enumerate(self.weights):
             net_weight = []
             for w, b in zip(weight, self.bias[i]):
                 net_weight.append(float(np.dot(activation, w) + b))  # wyliczanie iloczynu wag i wektora aktywacji
             net_weight = list(map(self.sigma_activation, net_weight))  # przetworzenie danych przez funkcję aktywacji
             activation = net_weight  # przekazanie danych do kolejnej warstwy
-            self.activation[i + 1] = activation  # zapisanie wartości aktywacji do propagacji wstecznej
+            self.activation[i + 1] = np.array(activation)  # zapisanie wartości aktywacji do propagacji wstecznej
         return self.softmax(activation)
 
     def backword_propagate(self, error):
-        NotImplemented
+        for i in reversed(range(len(self.weights))):
+            act = self.activation[i + 1]
+            delta = error * np.array(list(map(self.sigma_derivativ, act)))
+            delta_re = delta.reshape(delta.shape[0], -1).T
+            current_activations = self.activation[i]
+            current_activations = current_activations.reshape(current_activations.shape[0], -1)
+            self.derivatives[i] = np.matmul(current_activations, delta_re)
+            error = np.dot(delta, self.weights[i])
+
 
     def train(self, input, target):
-        NotImplemented
+        for i in range(0, 10):
+            sum_errors = 0
+            for _inp, _targ in zip(input, target):
+                output = self.forward_propagate(_inp)
+                error = -(_targ - output)
+                self.backword_propagate(error)
+                self.gradient_descent()
+                if error != 0:
+                    sum_errors += 1
+            print("Error: {} at epoch {}".format(sum_errors / len(input), i + 1))
 
     def gradient_descent(self, learningRate=0.001):
-        NotImplemented
+        #print("suma 1 {}".format(sum(self.weights[1][0])))
+        for i in range(len(self.weights)):
+            self.weights[i] += learningRate*self.derivatives[i].T
+            self.bias[i] += learningRate*self.bias[i]
+            #print(weights[i])
+            #print(bias[i])
+        #print("suma 2 {}".format(sum(self.weights[1])[0]))
+
+
+
 
     @staticmethod
     def sigma_activation(x):
