6b1f995a8b0d21297c79a4ce31cd29a90e8502b8
IwoZachary <61707966+IwoZachary@users.noreply.github.com>
2021-11-24 16:55:24 +0100

    Moments, adagrad, adaline

--- mlp.py
+++ mlp.py
@@ -1,8 +1,12 @@
 import numpy as np
+
 NUMBER_OF_OUTPUTS = 10
 EPSILON = 0.00000001
+
+
 class MLPv2:
-    def __init__(self, num_inputs, hidden_layers,learnig_rate, epoches, weight_loc, weight_scale, bias_loc, bias_scale):
+    def __init__(self, num_inputs, hidden_layers, learnig_rate, epoches, weight_loc, weight_scale, bias_loc,
+                 bias_scale):
         '''
 
         :param num_inputs: liczba parametrów wejściowych we wzorcu
@@ -16,38 +20,45 @@ class MLPv2:
         self.num_inputs = num_inputs  # ilość danych wejściowych
         self.layers = [self.num_inputs] + hidden_layers + [NUMBER_OF_OUTPUTS]  # warstwy z ilością neuronów w warstwie
         self.num_layers = len(self.layers)  # liczba warstw
-        self.activation = [] #wartość funkcji aktywacji
-        self.boost = [] #wartość funkcji pobudzenia
-        self.derivatives = [] #pochodne wag
+        self.activation = []  # wartość funkcji aktywacji
+        self.boost = []  # wartość funkcji pobudzenia
+        self.derivatives = []  # pochodne wag
         self.epoches = epoches
         weights = []
         bias = []
         previousDerivatives = []
-        previousBias = []
+        sumGradient = []
+        sumWeightDelta = []
+        previousWeightDelta = []
         for i in range(self.num_layers - 1):
             weights.append(
-                np.random.normal(loc=weight_loc, scale=weight_scale, size=(self.layers[i], self.layers[i+1])))
+                np.random.normal(loc=weight_loc, scale=weight_scale, size=(self.layers[i], self.layers[i + 1])))
             bias.append(
-                np.random.normal(loc=bias_loc, scale=bias_scale, size=( self.layers[i + 1])))
-            previousDerivatives.append(np.zeros(shape=(self.layers[i], self.layers[i+1])))
-        self.weights = weights #wagi
-        self.bias = bias #biasy
+                np.random.normal(loc=bias_loc, scale=bias_scale, size=(self.layers[i + 1])))
+            previousDerivatives.append(np.zeros(shape=(self.layers[i], self.layers[i + 1])))
+            sumGradient.append(np.zeros(shape=(self.layers[i], self.layers[i + 1])))
+            sumWeightDelta.append(np.zeros(shape=(self.layers[i], self.layers[i + 1])))
+            previousWeightDelta.append(np.zeros(shape=(self.layers[i], self.layers[i + 1])))
+        self.weights = weights  # wagi
+        self.bias = bias  # biasy
         self.previousDerivatives = previousDerivatives
-        
+        self.sumGradient = sumGradient
+        self.sumWeightDelta = sumWeightDelta
+        self.previousWeightDelta = previousWeightDelta
 
     def forward_propagate(self, input_x):
         act = input_x  # wartość pobudzenia w warstwie pierwszej
         self.activation.append(act)
         for i, weight in enumerate(self.weights):
             booster = np.dot(act, weight)
-            act = self.tanh(booster)
+            act = self.sigma_activation(booster)
             self.activation.append(act)
         return self.softmax(act)
 
     def backward_propagate(self, error):
         for i in reversed(range(len(self.weights))):
-            activation_upper = self.activation[i+1]
-            derivative = error * self.tanh_derivative(activation_upper)
+            activation_upper = self.activation[i + 1]
+            derivative = error * self.sigma_derivative(activation_upper)
             derivative_reshape = derivative.reshape(derivative.shape[0], -1).T
             activation_current = self.activation[i]
             activation_current = activation_current.reshape(activation_current.shape[0], -1)
@@ -74,53 +85,55 @@ class MLPv2:
                     t[_targ] = 1
                     error = t - output
                     self.backward_propagate(error)
-                    self.gradient_descent(ammount)
+                    #self.gradient_descent(ammount)
+                    #self.gradient_descent_with_momentum(0.7, ammount)
+                    #self.gradient_descent_with_nestov(0.7, ammount)
+                    #self.adaGrad()
+                    self.adaDelta(0.9)
                     sum_errors += error.mean() ** 2
                 if start:
                     startError = sum_errors / ammount
                     start = False
+                print("Error: {} at epoch {}".format(sum_errors / ammount, i + 1))
         endError = sum_errors / ammount
-        avverageStep = (startError - endError)/self.epoches
-        print(" {} {} {} ".format(startError, endError, avverageStep),end= "")
-                #print("Error: {} at epoch {}".format(sum_errors / ammount, i + 1))
+        avverageStep = (startError - endError) / self.epoches
+        print(" {} {} {} ".format(startError, endError, avverageStep), end="")
+
 
     def gradient_descent(self, ammount):
         for i in range(len(self.weights)):
-            self.weights[i] += self.learning_rate/ammount*self.derivatives[i]
-            self.bias[i] += self.learning_rate/ammount*self.derivatives[i].T.sum(axis =1)
-    
-    def gradient_descent_with_momentum(self, gamma):
+            self.weights[i] += self.learning_rate / ammount * self.derivatives[i]
+            self.bias[i] += self.learning_rate / ammount * self.derivatives[i].T.sum(axis=1)
+
+    def gradient_descent_with_momentum(self, gamma, ammount):
         for i in range(len(self.weights)):
-            delta_w = gamma*self.previousDerivatives[i]+ self.learning_rate*self.derivatives[i]
-            self.weights[i] -= delta_w 
-            self.bias[i] -= delta_w.T.sum(axis =1)
+            delta_w = gamma * self.previousDerivatives[i] + self.learning_rate/ammount * self.derivatives[i]
+            self.weights[i] += delta_w
+            self.bias[i] += delta_w.T.sum(axis=1)
             self.previousDerivatives[i] = delta_w
 
-    def gradient_descent_with_nestov(self, gamma):
+    def gradient_descent_with_nestov(self, gamma, ammount):
         for i in range(len(self.weights)):
-            delta_w = gamma*self.previousDerivatives[i]+ self.learning_rate*(self.weight[i] - self.derivatives[i])
-            self.weights[i] -= delta_w 
-            self.bias[i] -= delta_w.T.sum(axis =1)
+            delta_w = gamma * self.previousDerivatives[i] + self.learning_rate/ammount * (self.weights[i] - gamma*self.previousDerivatives[i])
+            self.weights[i] -= delta_w
+            self.bias[i] -= delta_w.T.sum(axis=1)
             self.previousDerivatives[i] = delta_w
-    
+
     def adaGrad(self):
         for i in range(len(self.weights)):
-            delta_w = self.learning_rate/(np.sqrt(self.derivatives[i]*self.derivatives[i].T)+ EPSILON)*self.derivatives[i]
-            self.weights[i] -= delta_w 
-            self.bias[i] -= delta_w.T.sum(axis =1)
-            
+            self.sumGradient[i] += self.derivatives[i] * self.derivatives[i]
+            delta_w = self.learning_rate/np.sqrt((self.sumGradient[i]+EPSILON))*self.derivatives[i]
+            self.weights[i] += delta_w
+            self.bias[i] += delta_w.T.sum(axis=1)
 
     def adaDelta(self, par):
-        st_p = 0 #dopasować wymiary do rozmiarów tablic
-        prev_delt = 0
         for i in range(len(self.weights)):
-            st = par* st_p + (1 - par)*np.power(self.derivatives[i])
-            delta_w = np.sqrt(prev_delt+EPSILON)/np.sqrt(st+EPSILON)*self.derivatives[i]
-            self.weights[i] -= delta_w 
-            self.bias[i] -= delta_w.T.sum(axis =1)
-            st_p = st
-            prev_delt = delta_w
-
+            self.sumGradient[i] = par*self.sumGradient[i] + (1-par)*(self.derivatives[i] * self.derivatives[i])
+            self.sumWeightDelta[i] = par*self.sumWeightDelta[i]+(1-par)*(self.previousWeightDelta[i]*self.previousWeightDelta[i])
+            delta_w = np.sqrt((self.sumWeightDelta[i]+EPSILON))/np.sqrt((self.sumGradient[i]+EPSILON))*self.derivatives[i]
+            self.weights[i] += delta_w
+            self.bias[i] += delta_w.T.sum(axis=1)
+            self.previousWeightDelta[i] = delta_w
 
 
     def validate(self, input_data, target):
@@ -131,13 +144,11 @@ class MLPv2:
             _value = res.index(_value)
             if _value == _target:
                 correct += 1
-        return correct/len(input_data)
-
-
+        return correct / len(input_data)
 
     @staticmethod
     def sigma_activation(x):
-        return 1/(1 + np.exp(-x))
+        return 1 / (1 + np.exp(-x))
 
     @staticmethod
     def sigma_derivative(x):
@@ -149,7 +160,7 @@ class MLPv2:
 
     @staticmethod
     def softplus(x):
-        return 1/(1 + np.exp(-x))
+        return 1 / (1 + np.exp(-x))
 
     @staticmethod
     def softmax(f_activation):
